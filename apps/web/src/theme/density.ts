2648200a48713f8436c8ffff85c169270bf8fa3f
illos <jimipringle@gmail.com>
2026-05-14 10:57:49 -0700

    feat(web/theme): public exports for theme module

---
+++ apps/web/src/theme/density.ts
@@ -0,0 +1,2 @@
+// apps/web/src/theme/density.ts
+export type { Density } from './ThemeProvider';
---
+++ apps/web/src/theme/index.ts
@@ -0,0 +1,3 @@
+// apps/web/src/theme/index.ts
+export { ThemeProvider } from './ThemeProvider';
+export type { Theme, Pack, Density, ThemeProviderProps } from './ThemeProvider';
